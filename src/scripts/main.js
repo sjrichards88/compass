14fcc23a3134ae621171c0c813a7f2dfadb63d80
Simon Richards <simon@designwildwest.com>
2016-07-22 17:05:51 +0100

    Profile changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,9 +11,13 @@ jQuery(function($) {
 	navigationItems = $('#cd-vertical-nav a'),
 	navigationItems2 = $('.navbar-nav li'),
 	loaded = true,
-	fixed_point = 0;
+	fixed_point = 0,
+	ms_debug = true,
+		// Responsive tracking
+	ms_current_bootstrap_size = "md";   // Should be lg/md/sm/xs
 
 	//On Load
+	track_window_size();
 	Adjust();
 	$('.navbar-stick .navbar').after("<div class='navbar-filler'></div>");
 	$.validate();
@@ -40,10 +44,16 @@ jQuery(function($) {
 
 	$(window).resize(function(){
 		Adjust();
+		resizeReviews();
+		track_window_size();
 	});
 
 	$(window).on('scroll', function() {
 		Adjust();
+	});
+
+	$(document).ready(function() {
+		resizeReviews();
 	});
 
 	// $('.sample-button').click(function(){
@@ -151,15 +161,26 @@ jQuery(function($) {
     	$('.profiles .toggleable').not(toggleDiv).removeClass('active');
     	$('.profiles .toggle').not($(this)).removeClass('active');
     	$(this).toggleClass('active');
-    	if (toggleText != null) {
-    		$(this).text(toggleText);
-    		$(this).data('toggle-text', currentText);
-    	}
     	if ($(toggleDiv).length > 0) {
     		$(toggleDiv).toggleClass('active');
     		if (scrollTo == null) scrollTo = toggleDiv;
     		smoothScroll(scrollTo);
+    	} 
+    });
+
+    $('.show-more-profiles').click(function() {
+    	var toggleDiv = $(this).data('toggle-class'),
+    	scrollTo = $(this).data('scroll-to'),
+    	currentText = $(this).text();
+    	toggleText = $(this).data('toggle-text');
+    	$(this).toggleClass('active');
+    	if (toggleText != null) {
+    		$(this).text(toggleText);
+    		$(this).data('toggle-text', currentText);
     	}
+    	$(this).closest('.reviews').find(toggleDiv).toggleClass('hidden');
+    	if (scrollTo == null) scrollTo = toggleDiv;
+	    smoothScroll(scrollTo);
     });
 
     /*----------------------
@@ -249,6 +270,70 @@ jQuery(function($) {
 
 			$(this).css('top', ((containerHeight / 2 - $(this).height() / 2) - contentHeight));
 		});
+	}
+
+	function resizeReviews() {
+		var maxHeight = -1;
+
+		$('.profiles .reviews').each(function() {
+			$(this).find('.review').each(function() {
+				maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+			});
+			$(this).find('.review').each(function() {
+				$(this).height(maxHeight);
+			});
+		});
+	}
+
+	/**
+	 * Monitors the size of the window and detects if the bootstrap template changes size
+	 * Sets some global variables to help us keep track of the window size and bootstrap template being displayed
+	 * @returns {void}
+	 */
+	function track_window_size()
+	{
+	    // Keep track of window/document height
+	    ms_window_height = $(window).height();
+	    ms_window_width = $(window).width();
+	    ms_document_height = $(document).height();
+	    ms_document_width = $(document).width();
+
+	    // Keep track of bootstrap template size
+	    var old_bootstrap_size = ms_current_bootstrap_size;
+	    if($('#bootstrap_detector .visible-xs').is(':visible'))
+	    {
+	        ms_current_bootstrap_size = "xs";
+	    }
+	    // else if($('#bootstrap_detector .visible-ms').is(':visible'))
+	    // {
+	    //     ms_current_bootstrap_size = "ms";
+	    // }
+	    else if($('#bootstrap_detector .visible-sm').is(':visible'))
+	    {
+	        ms_current_bootstrap_size = "sm";
+	    }
+	    else if($('#bootstrap_detector .visible-md').is(':visible'))
+	    {
+	        ms_current_bootstrap_size = "md";
+	    }
+	    else if($('#bootstrap_detector .visible-lg').is(':visible'))
+	    {
+	        ms_current_bootstrap_size = "lg";
+	    }
+
+	    // Trigger a bootstrapResize?
+	    if(old_bootstrap_size !== ms_current_bootstrap_size)
+	    {
+	        // Debug
+	        if(ms_debug)
+	        {
+	            console.log('track_window_size: '+ms_current_bootstrap_size);
+	        }
+
+	        $(window).trigger('bootstrapResize');
+
+	    }
+	    return (old_bootstrap_size !== ms_current_bootstrap_size);
 	}
 
 
